8ba90703b97f0a5d68b9461ac86dd8b9ce0d94c0
FebrianPratamaS <pratamasfebrian@gmail.com>
2024-09-25 09:11:31 +0800

    perulangan 1
    perulangan 1

---
+++ Pertemuan-4/perulangan for.py
@@ -0,0 +1,41 @@
+# ulang = 10
+# for i in range(ulang):
+#     print("Perulangan ke-" + str(i))
+
+# for fufufafa in range(2,12,-2):
+#     print("halo")
+# print("hai")
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i)
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in simpan:
+#     print(i)
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i, menu in enumerate (simpan, start=1):
+#     print(f"{i}. {menu}")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in range(len(simpan)):
+#     print(f"{i+1}. {simpan[i]}")
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# makanan = ["mie", "sop", "bakso"]
+# minuman = ["es teh", "air putih", "es jeruk"]
+
+# for i in makanan:
+#     for j in minuman:
+#         print(f"{i} & {j}")
